3aef23ea1b913c02341948a499c5ebde12c3db61
surfvault <logan@surf-vault.com>
2025-04-24 22:51:58 -0400

    send pusher on upload complete (#16)

--- shared/pusher_client.ts
+++ shared/pusher_client.ts
@@ -13,9 +13,9 @@ export class PusherClient {
   constructor() {
     this.pusher = new pusher({
       appId: process.env.PUSHER_APP_ID,
-      key: process.env.PUSHER_KEY,
-      secret: process.env.PUSHER_SECRET,
-      cluster: process.env.PUSHER_CLUSTER,
+      key: process.env.PUSHER_APP_KEY,
+      secret: process.env.PUSHER_APP_SECRET,
+      cluster: process.env.PUSHER_APP_CLUSTER,
     });
   }
 
